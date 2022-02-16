7e80d185104296f3f6a9406cca9b47bc09ceab72
John <deshanj@resa.net>
2022-02-16 13:51:20 -0500

    Harder, Better, Faster, Stronger

--- js/content.js
+++ js/content.js
@@ -1,28 +1,7 @@
-// Had to add a delay so that the video could load first
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
 const likeFunction = async () => {
-    await delay(10000)
-    var isLiked = function (btn) {
-        for (var btn of document.getElementsByTagName("yt-formatted-string"))
-            if (btn.getAttribute("class"))
-                if (btn.getAttribute("class").includes("active"))
-                    return btn
-    }
+    let btn = document.querySelector("#top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(1)");
+    if(btn && !btn.classList.contains("style-default-active"))
+        btn.click();
+}
 
-    while (!isLiked()) {
-        for (var btn of document.getElementsByTagName("button"))
-            if (btn.getAttribute("aria-label"))
-                if (btn.getAttribute("aria-label").includes("like this video along"))
-                    btn.click()
-    }
-};
-
-const monitorVideoChange = setInterval(function() {
-    if (document.querySelector('#container > h1')) {
-        //clearInterval(monitorVideoChange);
-        likeFunction();
-    }
-}, 5000)
-
-window.onload = likeFunction() && monitorVideoChange()
+window.setInterval(likeFunction, 10000);

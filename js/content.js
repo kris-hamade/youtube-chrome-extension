f6ccb917400b76d0215c18b75266942c29d738ac
Kris Hamade <khamade22@gmail.com>
2023-03-20 11:40:47 -0400

    Fixed a Bug with Like Detection

--- js/content.js
+++ js/content.js
@@ -1,11 +1,14 @@
 const likeFunction = async () => {
-    const xpath = `//*[@id="segmented-like-button"]/ytd-toggle-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]`;
-    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    
-    const likeButton = result.singleNodeValue;
-    
-    const likeButtonStatus = document.querySelector('.yt-spec-button-shape-next.yt-spec-button-shape-next--tonal.yt-spec-button-shape-next--mono');
-    
+    const buttonXpath = `//*[@id="segmented-like-button"]/ytd-toggle-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]`;
+    const buttonResult = document.evaluate(buttonXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+    const isLikedXpath = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-segmented-like-dislike-button-renderer/div[1]/ytd-toggle-button-renderer/yt-button-shape/button";
+    const xLikedResult = document.evaluate(isLikedXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+    const likeButton = buttonResult.singleNodeValue;
+
+    const likeButtonStatus = xLikedResult.singleNodeValue;
+
     if (likeButtonStatus && likeButtonStatus.getAttribute('aria-pressed') !== 'true') {
       likeButton.click();
     }

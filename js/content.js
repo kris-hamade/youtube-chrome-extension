369dbce7aa9e9c027e68abe0df3583db2593286b
Kris Hamade <khamade22@gmail.com>
2023-03-20 11:03:41 -0400

    Updated Code to Work with YouTubes New Changes

--- js/content.js
+++ js/content.js
@@ -1,7 +1,14 @@
 const likeFunction = async () => {
-    let btn = document.querySelector("#top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(1)");
-    if(btn && !btn.classList.contains("style-default-active"))
-        btn.click();
+    const xpath = `//*[@id="segmented-like-button"]/ytd-toggle-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]`;
+    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    
+    const likeButton = result.singleNodeValue;
+    
+    const likeButtonStatus = document.querySelector('.yt-spec-button-shape-next.yt-spec-button-shape-next--tonal.yt-spec-button-shape-next--mono');
+    
+    if (likeButtonStatus && likeButtonStatus.getAttribute('aria-pressed') !== 'true') {
+      likeButton.click();
+    }
 }
 
 window.setInterval(likeFunction, 10000);
